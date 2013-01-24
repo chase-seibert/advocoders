b7c6e4919121ebc1b8f6957ee7268de9beed9252
Chase Seibert <chase.seibert+github@gmail.com>
2013-01-23 22:05:07 -0800

    pass number 2 at celery

--- advocoders/models.py
+++ advocoders/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
 from social_auth.models import UserSocialAuth
-import signals  # DO NOT REMOVE; register singals
 
 
 class Company(models.Model):
--- advocoders/urls.py
+++ advocoders/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+import signals  # DO NOT REMOVE; register signals
 
 
 admin.autodiscover()
